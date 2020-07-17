c23b120b4485888ca591d0c23afc2f9739452283
chiragrathodj <rathodchiragj@gmail.com>
2020-07-17 16:43:15 +0530

    refactoring logic

--- src/main/java/calculator/util/StringCalculator.java
+++ src/main/java/calculator/util/StringCalculator.java
@@ -4,22 +4,35 @@ import java.util.Arrays;
 
 public class StringCalculator {
 
-    public static int sum(String number) {
-        if(number.isEmpty())
+    private String delimiter;
+    private String numbers;
+
+    private StringCalculator(String delimiter, String numbers) {
+        this.delimiter = delimiter;
+        this.numbers = numbers;
+    }
+
+    public static int sum(String input) {
+        if(input.isEmpty())
             return 0;
 
-        String delimiter = ",|\n";
+        return parseNumbersInput(input).getSum();
+    }
 
-        if(number.startsWith("//")) {
-            String[] splitWithDemil = number.split("\n", 2);
-            delimiter = splitWithDemil[0].substring(2);
-            number = splitWithDemil[1];
-        }
-
-        return Arrays.stream(number.split(delimiter))
+    private int getSum() {
+        return Arrays.stream(numbers.split(delimiter))
                     .filter((n) -> !n.isEmpty())
                     .map(String::trim)
                     .mapToInt(Integer::parseInt)
                     .sum();
+    }
+
+    private static StringCalculator parseNumbersInput(String input) {
+        if (input.startsWith("//")) {
+            String[] splitWithDemil = input.split("\n", 2);
+            return new StringCalculator(splitWithDemil[0].substring(2), splitWithDemil[1]);
+        } else {
+            return new StringCalculator(",|\n", input);
+        }
     }
 }
