f453094d9f382580f1bc7c1aadad860b114fe699
chiragrathodj <rathodchiragj@gmail.com>
2020-07-17 15:55:01 +0530

    case: shouldReturnSumOfTwoNumbersDelimitedByCommas

--- src/main/java/calculator/util/StringCalculator.java
+++ src/main/java/calculator/util/StringCalculator.java
@@ -7,14 +7,11 @@ public class StringCalculator {
     public static int sum(String number) {
         if(number.isEmpty())
             return 0;
-        else if(number.contains(",")){
-            return Arrays.stream(number.split(","))
+
+        return Arrays.stream(number.split(","))
                     .filter((n) -> !n.isEmpty())
                     .map(String::trim)
                     .mapToInt(Integer::parseInt)
                     .sum();
-        } else {
-            return Integer.parseInt(number);
-        }
     }
 }
